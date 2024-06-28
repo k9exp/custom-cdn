94dedf7e0964aaf5409f8a66064a102f97ef0080
Kunal Singh <kunalsin9h@gmail.com>
2024-06-28 19:49:09 +0530

    feat

--- infra/variables.tf
+++ infra/variables.tf
@@ -23,31 +23,6 @@ variable "vm_map" {
 		location = "West Europe"
 		size = "Standard_B1s"
 	}
-
-	"vm4" = {
-		name = "australiacentral"
-		location = "Australia Central"
-		size = "Standard_B1s"
-	}
-
-
-	"vm5" = {
-		name = "japaneast"
-		location = "Japan East"
-		size = "Standard_B1s"
-	}
-
-	"vm6" = {
-		name = "southafricanorth"
-		location = "South Africa North"
-		size = "Standard_B1s"
-	}
-
-	"vm7" = {
-		name = "brazilsouth"
-		location = "Brazil South"
-		size = "Standard_B1s"
-	}
   }
 }
 
